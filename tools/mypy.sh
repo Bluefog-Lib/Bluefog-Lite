29e1bfea3359cb91574249191cf24e612c742add
ybc <bichengying@gmail.com>
2022-02-12 21:04:16 -0800

    Update mypy.sh

--- tools/mypy.sh
+++ tools/mypy.sh
@@ -2,6 +2,7 @@
 
 # Usage:  (under the root directory)
 #   tools/mypy.sh [--version]
+set -x
 
 mypy_files=$(find bluefoglite -name "*.py" ! -name '*_pb2.py')
 mypy --config-file=tools/.mypy.in "$@" ${mypy_files}
