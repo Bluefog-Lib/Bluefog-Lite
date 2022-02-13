b7022da099304125534c11f9ab4a200355c789ec
ybc <bichengying@gmail.com>
2022-02-12 22:44:00 -0800

    Add just mypy CI again

--- tools/mypy.sh
+++ tools/mypy.sh
@@ -3,6 +3,8 @@
 # Usage:  (under the root directory)
 #   tools/mypy.sh [--version]
 
-# mypy_files=$(find bluefoglite -name "*.py" ! -name '*_pb2.py')
-# mypy --config-file=tools/.mypy.in "$@" ${mypy_files}
-mypy --config-file=tools/.mypy.in bluefoglite/*.py bluefoglite/common/*.py bluefoglite/common/collective_comm/*.py
+
+# I don't know why this approach cannot revmove the check on *_pb2.py files
+# in Github CI. But it works perfectly in local machine
+mypy_files=$(find bluefoglite -name "*.py" ! -name '*_pb2.py')
+mypy --config-file=tools/.mypy.in "$@" ${mypy_files}
