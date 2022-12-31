9361c601d6df2eecaf0cc796c344b412ceb0446e
ybc <bichengying@gmail.com>
2022-12-30 17:07:54 -0800

    also install protobuf when doing mypy in CI

--- tools/mypy.sh
+++ tools/mypy.sh
@@ -3,7 +3,6 @@
 # Usage:  (under the root directory)
 #   tools/mypy.sh [--version]
 
-mypy --install-types
 # I don't know why this approach cannot revmove the check on *_pb2.py files
 # in Github CI. But it works perfectly in local machine
 mypy_files=$(find bluefoglite -name "*.py" ! -name '*_pb2.py')
